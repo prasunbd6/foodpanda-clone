8fa3fa5dd1f4398a087aa8bf7ae16769bda0b306
prasunbd6 <prasunbd6@gmail.com>
2023-10-31 23:09:55 +0600

    v12
    update Navbar icons

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -11,30 +11,30 @@ const Navbar = () => {
         <div className="w-full h-auto md:w-11/12 md:h-20 flex flex-wrap justify-between items-center bg-white py-1">
           {/* User Logo & Brand */}
 
-          <i className="w-2/12 md:w-2/12 px-1 text-pink-500 text-xl font-bold md:hidden order-1">
+          <i className="w-2/12 md:w-1/12 px-1 text-pink-500 text-xl font-bold md:hidden order-1">
             <CiUser size={30} />
           </i>
 
-          {/* Brand Name */}
+          {/* Logo & Brand Name */}
           <div className="md:w-auto flex items-center  text-pink-500 gap-2 md:order-2 order-2">
-            <i className="p-2 rounded-lg bg-pink-500">
-              <SiFoodpanda size={25} className="bg-white rounded-xl" />
+            <i className="p-1 rounded-lg bg-pink-500">
+              <SiFoodpanda size={23} className="bg-white rounded-xl" />
             </i>
-            <p className=" font-bold text-2xl">foodpanda</p>
+            <p className=" font-bold text-xl">foodpanda</p>
           </div>
 
           
           {/* login, Signup & language */}
-          <ul className="hidden md:w-auto md:h-10 md:flex md:justify-end md:items-center md:py-1 md:text-center md:gap-5 md:order-4">
+          <ul className="hidden md:w-3/12 md:h-10 md:flex md:justify-end md:items-center md:py-1 md:text-center md:gap-1 md:order-4">
             <li>login</li>
             <li>Sign Up</li>
             <li>Language</li>
           </ul>
 
           {/* Heart & Shopping Logo */}
-          <ul className="w-2/12 md:w-auto flex justify-end items-center text-pink-500 gap-4 p-1 md:order-5 order-3">
-          <li><AiOutlineHeart size={30} /></li>
-          <li><HiOutlineShoppingBag size={30} /></li>
+          <ul className="w-2/12 md:w-24 flex justify-end md:justify-around items-center text-pink-500 gap-4 p-1 md:order-5 order-3">
+          <li className="rounded md:rounded flex justify-center hover:rounded-full hover:bg-pink-100 md:p-1"><AiOutlineHeart size={30} /></li>
+          <li className="rounded md:rounded flex justify-center hover:rounded-full hover:bg-pink-100 md:p-1"><HiOutlineShoppingBag size={30} /></li>
           </ul>
 
           {/* Map Logo & Address */}
