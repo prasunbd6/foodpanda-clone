55f399ebeb76cbed739f7d69a5cfd0d12d7f3edc
prasunbd6 <prasunbd6@gmail.com>
2023-11-08 16:40:19 +0600

    v31
    still working on radio button in Delivery Page

--- src/Components/Delivery.js
+++ src/Components/Delivery.js
@@ -84,37 +84,55 @@ const Delivery = () => {
               <p className="filter-heading-text">Sort by</p>
 
               <ul>
-                <li>
-                  <label className=" flex justify-start items-center gap-3 group">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="icon icon-tabler icon-tabler-circle-dot-filled border rounded-full border-pink-500 text-pink-500 group-hover:border-2 group-hover:border-pink-500"
-                      width="18"
-                      height="18"
-                      viewBox="0 0 24 24"
-                      stroke-width="2"
-                      stroke="currentColor"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                      <path
-                        d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-5 6.66a2 2 0 0 0 -1.977 1.697l-.018 .154l-.005 .149l.005 .15a2 2 0 1 0 1.995 -2.15z"
-                        stroke-width="0"
-                        fill="currentColor"
-                      ></path>
-                    </svg>
+                {sortby.map((y) => {
+                  return (
+                    <>
+                      <li>
+                        <label className="flex items-center py-1 gap-2 group">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className=" w-5 h-5 border border-pink-500 rounded-full 
+                            focus:outline-none 
+                            checked:border-opacity-10 
+                            checked:bg-pink-500 
+                            group-hover:rounded-full 
+                            group-hover:ring-2
+                            group-hover:text-pink-500  
+                            group-hover:ring-pink-500
+                            group-checked:text-pink-500"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                            />
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M9 9.563C9 9.252 9.252 9 9.563 9h4.874c.311 0 .563.252.563.563v4.874c0 .311-.252.563-.563.563H9.564A.562.562 0 019 14.437V9.564z"
+                            />
+                          </svg>
 
-                    <input
-                      type="radio"
-                      className="appearance-none group-checked:text-pink-500"
-                      checked
-                    />
+                          <input
+                            type="radio"
+                            name="status"
+                            id={y.id}
+                            checked
+                            className="appearance-none"
+                          />
 
-                    <span className="group-hover:text-pink-500">test</span>
-                  </label>
-                </li>
+                          <span className="group-hover:text-pink-500">
+                            {y.type}
+                          </span>
+                        </label>
+                      </li>
+                    </>
+                  );
+                })}
 
                 {sortby.map((y) => {
                   return (
@@ -126,9 +144,18 @@ const Delivery = () => {
                             name="status"
                             id={y.id}
                             checked
-                            className="filter-list-label-round peer/draft"
+                            className="appearance-none w-5 h-5 border border-pink-500 rounded-full 
+                            focus:outline-none 
+                            checked:border-opacity-10 
+                            checked:bg-pink-600 
+                            group-hover:rounded-full 
+                            group-hover:ring-2
+                            group-hover:text-pink-500  
+                            group-hover:ring-pink-600
+                            group-checked:text-pink-600"
                           />
-                          <span className="group-hover:text-pink-500 peer-checked/draft:text-pink-500">
+
+                          <span className="group-hover:text-pink-500 ">
                             {y.type}
                           </span>
                         </label>
