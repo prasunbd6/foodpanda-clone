6ec811e553e901c546567d82212d7fa83d36f54e
prasunbd6 <prasunbd6@gmail.com>
2023-11-09 11:52:11 +0600

    v33
    update body (Delivery page)

--- src/Components/Delivery.js
+++ src/Components/Delivery.js
@@ -65,6 +65,33 @@ const Delivery = () => {
     { id: 2, offers: "Deals" },
   ];
 
+  const content = [
+    {
+      id: 1,
+      heading: "Order food online in Chittagong",
+      details:
+        "If you thought Bangladeshi cuisine was all about fish and rice, think again. While seafood and grains might serve as the staples for many a dish, there's much more to discover from this rich culinary heritage. In Chittagong, you'll find a remarkable range of regional specialities alongside the signatures of the best of Bangladeshi cooking. Whether you're looking to indulge with hot curries loaded with tender meat or please your palate with deliciously fresh tropical fruit recipes, you're well catered when it comes to food delivery in Chittagong.",
+    },
+    {
+      id: 2,
+      heading: "Make ordering online easy with foodpanda",
+      details:
+        "When hunger strikes, make foodpanda your first port of call. Whether you're home late and too tired to cook or stuck at the office and in need of nourishment, our user-friendly platform allows you to order food delivery in no time at all. To get started, head out to the foodapanda website or launch the app. Then, enter your area and search to find restaurants near you delivering to your location. You'll then see an overview of what's on offer. Simply click to open that restaurants delivery page and browse the full menu. Tap or click to add desired items to your basket, then head to the online checkout to pay and finalise your order. Once you've handled that part, the rest is taken care of, meaning you can relax as your food is prepared and delivered direct to your door.",
+    },
+    {
+      id: 3,
+      heading: "Enjoy first class food from the best Chittagong restaurants",
+      details:
+        "Whatever your appetite, there's plenty of choice when it comes to food takeaway in Chittagong. Here at foodpanda, our expert taste team have scoured eateries throughout the city to ensure you enjoy premium choice from the best restaurants around. Looking for homegrown dishes with homegrown flavours? Sink your teeth into Indian and Bengali specialities from the likes of Premium Sweets, Ahaar and Z Kitchen. Prefer to chow down on Chinese dishes? Find what you're looking for at Royal Kitchen and Korai Gost, or opt for fragrant Thai classics from Street 11 and Xenial. There are also rich pickings when it comes to Western dishes and fast food favourites.",
+    },
+    {
+      id: 4,
+      heading: "Delve into a diverse range of culinary delights",
+      details:
+        "Like elsewhere in Bangladesh, Chittagong offers up an eclectic offering when it comes to food. You'll never struggle to find first-rate fish dishes here, with the port city of Chittagong particularly renowned for its standout seafood. Classic dried fish and crab dishes are popular here, while more exotic alternatives like stingray are on hand to broaden your culinary horizons. If it's a curry you're craving, there's few better places in Bangladesh to be. Curries here are famously spicy and rich in flavour, with succulent beef the go-to. South Asian cuisine is also well represented, with plenty of premier Chinese and Thai eateries to entice.",
+    },
+  ];
+
   return (
     <>
       <div className="flex flex-col justify-center items-center mt-36 pt-1 md:mt-28">
@@ -76,7 +103,9 @@ const Delivery = () => {
 
       <div className="h-auto flex flex-col justify-center items-center mt-0 gap-4">
         <div className=" w-11/12 h-auto flex flex-wrap justify-center items-start text-slate-600 md:justify-start md:items-start md:mt-5">
-          <div className="hidden md:mt-3 md:w-64 md:h-[840px] md:flex md:flex-col md:justify-start md:items-start md:px-4 md:gap-5 md:border md:border-l-2 rounded-s-2xl md:pt-5 md:overflow-y-scroll">
+         
+        {/* Left Side Bar */}
+        <div className="hidden md:mt-3 md:w-64 md:h-[840px] md:flex md:flex-col md:justify-start md:items-start md:px-4 md:gap-5 md:border md:border-l-2 rounded-s-2xl md:pt-5 md:overflow-y-scroll">
             <p className="text-xl font-bold">Filters</p>
 
             {/*Sort by*/}
@@ -94,8 +123,8 @@ const Delivery = () => {
                             name="status"
                             id={y.id}
                             checked
-                            className="appearance-none w-5 h-5 border ring-inset ring-4 ring-white border-pink-500 rounded-full 
-                            sort-checked sort-checked-focus sort-focus sort-group-hover
+                            className="appearance-none w-5 h-5 border ring-inset ring-4 ring-white border-pink-500 rounded-full transition ease-in-out duration-300
+                            sort-checked sort-checked-focus sort-focus sort-group-hover sort-hover-active
                            "
                           />
 
@@ -134,7 +163,7 @@ const Delivery = () => {
                           <input
                             type="checkbox"
                             value={o.offers}
-                            className="filter-list-label-square"
+                            className="filter-list-label-square transition ease-in-out duration-300"
                           />
                           <p className="group-hover:text-pink-500">
                             {o.offers}
@@ -162,9 +191,12 @@ const Delivery = () => {
                             type="checkbox"
                             id={x.id} // Add an id to the checkbox
                             value={x.cname}
-                            className="filter-list-label-square"
+                            className=" appearance-none w-5 h-5 border-2 border-pink-500  rounded-md ring-inset 
+                            transition ease-in-out duration-300
+                            sort-group-hover
+                            sort-marked
+                            "
                           />
-
                           <p className="group-hover:text-pink-500">{x.cname}</p>
                         </label>
                       </li>
@@ -176,12 +208,17 @@ const Delivery = () => {
           </div>
 
           {/* Body */}
-          <div className="md:w-[980px] md:h-auto mt-3 bg-pink-500">
-            <div className=" flex flex-wrap justify-start items-center px-6">
-                Working Area
+          <div className="w-full h-auto flex flex-wrap justify-start gap-6  md:w-4/6 md:px-5 lg:w-9/12 lg:px-10  xl:w-10/12 xl:px-16">
+            {content.map((con) => {
+              return (
+                <>
+                    <h1 className=" font-bold text-xl md:text-2xl lg:xl:text-3xl" key={con.id}>{con.heading}</h1>
+                    <p className="text-slate-600 text-lg md:lg:xl:text-2xl">{con.details}</p>
+                  
+                </>
+              );
+            })}
             </div>
-          
-          </div>
         </div>
       </div>
     </>
