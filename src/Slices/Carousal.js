126d4e591ab74e967b0f378d7080f5ccf25ed066
prasunbd6 <prasunbd6@gmail.com>
2023-11-16 16:53:08 +0600

    v46
    react-slick

--- src/Slices/Carousal.js
+++ src/Slices/Carousal.js
@@ -1,50 +1,13 @@
-import { BiSolidLeftArrow, BiSolidRightArrow } from "react-icons/bi";
-import { BsCircleFill } from "react-icons/bs";
 
-const Carousal = ({ slides }) => {
-  return(
+const Carousal = ({ slides}) => {
+
+  
+
+  return (
     <>
-
-    {
-      slides.map(
-        data=>{
-          return(
-            <>
-            <div key={data.id} className="w-96 h-auto flex flex-col justify-start bg-green-100">
-            <div className="">
-              <img src={data.photo_url} className=" bg-cover bg-center" alt=""/>
-            </div>
     
-            <div className="">
-              <div className="flex justify-between">
-                <p className=" font-bold text-md">{data.restaurant_name}</p>
-                <div className="flex">
-                  <i>icon</i>
-                  <h5>{data.ratings}</h5>
-                  <p>({data.subscriber} +)</p>
-                </div>
-              </div>
-            </div>
-    
-            <div className="">
-              <p>৳৳৳ {data.food_type}</p>
-            </div>
-    
-            <div className="">
-              <p>Food {data.service_charge}</p>
-            </div>
-    
-    
-          </div>
-            </>
-          )
-        }
-      )
-    }
-      
     </>
-  ) 
-
+  );
 };
 
 export default Carousal;
--- src/Slices/RightSideBody.js
+++ src/Slices/RightSideBody.js
@@ -1,65 +1,135 @@
-import React from "react";
-import Carousal from "./Carousal";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 
 const RightSideBody = ({ RiEqualizerLine, content, AiOutlineSearch }) => {
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  };
+
   const slides = [
     {
-      id:1,
-      photo_url:"https://images.pexels.com/photos/1448721/pexels-photo-1448721.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      restaurant_name:"Govinda's",
-      ratings:3.5/5,
-      subscriber:1000,
-      food_type:"Bangladeshi",
-      service_charge:"delivery"
+      id: 1,
+      photo_url:
+        "https://images.pexels.com/photos/1448721/pexels-photo-1448721.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      restaurant_name: "Govinda's",
+      ratings: 3.5 / 5,
+      subscriber: 1000,
+      food_type: "Bangladeshi",
+      service_charge: "delivery",
     },
 
     {
-      id:2,
-      photo_url:"https://images.pexels.com/photos/8471703/pexels-photo-8471703.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      restaurant_name:"Radison Blu",
-      ratings:4/5,
-      subscriber:2000,
-      food_type:"Bangladeshi",
-      service_charge:"delivery"
+      id: 2,
+      photo_url:
+        "https://images.pexels.com/photos/8471703/pexels-photo-8471703.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      restaurant_name: "Radison Blu",
+      ratings: 4 / 5,
+      subscriber: 2000,
+      food_type: "Bangladeshi",
+      service_charge: "delivery",
     },
 
     {
-      id:3,
-      photo_url:"https://images.pexels.com/photos/1426715/pexels-photo-1426715.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      restaurant_name:"Rodela Bikel",
-      ratings:3.5/5,
-      subscriber:5000,
-      food_type:"Bangladeshi",
-      service_charge:"delivery"
+      id: 3,
+      photo_url:
+        "https://images.pexels.com/photos/1956974/pexels-photo-1956974.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      restaurant_name: "Rodela Bikel",
+      ratings: 3.5 / 5,
+      subscriber: 5000,
+      food_type: "Bangladeshi",
+      service_charge: "delivery",
     },
 
     {
-      id:4,
-      photo_url:"https://images.pexels.com/photos/2814828/pexels-photo-2814828.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      restaurant_name:"Laziza",
-      ratings:4.5/5,
-      subscriber:3000,
-      food_type:"Bangladeshi",
-      service_charge:"delivery"
+      id: 4,
+      photo_url:
+        "https://images.pexels.com/photos/2814828/pexels-photo-2814828.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      restaurant_name: "Laziza",
+      ratings: 4.5 / 5,
+      subscriber: 3000,
+      food_type: "Bangladeshi",
+      service_charge: "delivery",
     },
 
     {
-      id:5,
-      photo_url:"https://images.pexels.com/photos/4393021/pexels-photo-4393021.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      restaurant_name:"Laham's",
-      ratings:3/5,
-      subscriber:4000,
-      food_type:"Bangladeshi",
-      service_charge:"delivery"
+      id: 5,
+      photo_url:
+        "https://images.pexels.com/photos/4393021/pexels-photo-4393021.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      restaurant_name: "Laham's",
+      ratings: 3 / 5,
+      subscriber: 4000,
+      food_type: "Bangladeshi",
+      service_charge: "delivery",
     },
-  
+
+    {
+      id: 6,
+      photo_url:
+        "https://images.pexels.com/photos/1448721/pexels-photo-1448721.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      restaurant_name: "Govinda's",
+      ratings: 3.5 / 5,
+      subscriber: 1000,
+      food_type: "Bangladeshi",
+      service_charge: "delivery",
+    },
+
+    {
+      id: 7,
+      photo_url:
+        "https://images.pexels.com/photos/8471703/pexels-photo-8471703.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      restaurant_name: "Radison Blu",
+      ratings: 4 / 5,
+      subscriber: 2000,
+      food_type: "Bangladeshi",
+      service_charge: "delivery",
+    },
+
+    {
+      id: 8,
+      photo_url:
+        "https://images.pexels.com/photos/1956974/pexels-photo-1956974.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      restaurant_name: "Rodela Bikel",
+      ratings: 3.5 / 5,
+      subscriber: 5000,
+      food_type: "Bangladeshi",
+      service_charge: "delivery",
+    },
+
+    {
+      id: 9,
+      photo_url:
+        "https://images.pexels.com/photos/2814828/pexels-photo-2814828.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      restaurant_name: "Laziza",
+      ratings: 4.5 / 5,
+      subscriber: 3000,
+      food_type: "Bangladeshi",
+      service_charge: "delivery",
+    },
+
+    {
+      id: 10,
+      photo_url:
+        "https://images.pexels.com/photos/4393021/pexels-photo-4393021.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      restaurant_name: "Laham's",
+      ratings: 3 / 5,
+      subscriber: 4000,
+      food_type: "Bangladeshi",
+      service_charge: "delivery",
+    },
   ];
 
-
+ 
 
   return (
     <>
-      <div className=" md:w-[460px] lg:w-[680px] xl:w-auto h-auto flex flex-col justify-start items-center gap-4">
+      <div className=" md:w-[460px] lg:w-[680px] xl:w-[900px]  2xl:w-[1160px] h-auto flex flex-col justify-start items-center gap-4">
         {/************************************************* Working Area - START *****************************************************/}
 
         <div className="w-11/12 h-auto flex justify-between md:justify-start items-center gap-4 lg:gap-5 py-2 px-1 mt-5">
@@ -75,12 +145,12 @@ const RightSideBody = ({ RiEqualizerLine, content, AiOutlineSearch }) => {
                 name="search"
                 autoComplete="off"
                 placeholder="Search for Cuisine"
-                className="w-[360px] md:w-[380px] h-10 md:h-12 bg-slate-50 px-10 border rounded-3xl md:rounded-sm focus:outline-none focus:ring-2 focus:ring-pink-400 md:focus:ring-2 md:focus:ring-sky-100"
+                className="w-[280px] md:w-[380px] h-10 md:h-12 bg-slate-50 px-10 border rounded-3xl md:rounded-sm focus:outline-none focus:ring-2 focus:ring-pink-400 md:focus:ring-2 md:focus:ring-sky-100"
               />
             </label>
           </form>
 
-          <div className=" flex justify-center items-center border rounded-full py-2 px-4 gap-2 focus:outline-none ring-2 ring-slate-200 hover:ring-slate-300">
+          <div className=" flex justify-center items-center border rounded-full py-1 px-2 gap-2 focus:outline-none ring-2 ring-slate-200 hover:ring-slate-300">
             <span>
               <RiEqualizerLine size={18} />
             </span>
@@ -89,10 +159,38 @@ const RightSideBody = ({ RiEqualizerLine, content, AiOutlineSearch }) => {
           {/************** Search Content (SM Device) - END ******************/}
         </div>
 
-        <div className="w-11/12 h-auto m-auto p-1">
-          <div className="flex items-center gap-2">
-          <Carousal slides={slides}/>
-          </div> 
+{/*Slider*/}
+        <div className="w-full md:w-11/12 mx-auto">
+        
+<div className="mt-16">
+    <Slider {...settings}>
+    {
+      slides.map(
+        (d)=>{
+          return(
+            <>
+              <div className=" bg-slate-50 h-[280px] text-black rounded-xl">
+                <div className=" h-56 rounded-t-xl bg-indigo-500 flex justify-center items-center">
+                  <img src={d.photo_url} alt="" className="h-44 w-44 rounded-full"/>
+                </div>
+
+                <div className="flex flex-col justify-center items-center gap-4 p-4">
+                  <p className=" text-xl font-semibold">{d.restaurant_name}</p>
+                  <p>Ratings {d.ratings}</p>
+                  <button className="mx-auto bg-indigo-500 text-white text-lg px-4 py-1 rounded-xl">Read More</button>
+                </div>
+                
+                
+              </div>
+            </>
+          )
+        }
+      )
+    }
+    </Slider>
+</div>
+
+          
         </div>
 
         <div className="w-full md:w-[475px] lg:w-[650px] xl:w-[915px] 2xl:w-[1148px]  h-auto flex flex-col flex-wrap justify-start gap-10 md:px-4">
