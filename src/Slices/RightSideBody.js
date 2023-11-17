fe2715e028fa4c3ecaa6e9283525bc8a4561e163
Prasun Barua <prasunbd6@gmail.com>
2023-11-17 22:07:23 +0600

    v52
    process react-slick

--- src/Slices/RightSideBody.js
+++ src/Slices/RightSideBody.js
@@ -3,11 +3,62 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const RightSideBody = ({ RiEqualizerLine, content, AiOutlineSearch }) => {
-  const settings = {
+
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "red" }}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "green" }}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  var settings = {
+    dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 2,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   const slides = [
@@ -163,13 +214,12 @@ const RightSideBody = ({ RiEqualizerLine, content, AiOutlineSearch }) => {
                   <div className="w-32 flex flex-col">
                     <img src={d.photo_url} alt="" />
                     <p className="text-center">{d.restaurant_name}</p>
+                    <bottom></bottom>
                   </div>
                 </>
               );
             })}
           </Slider>
-
-          
         </div>
 
         
