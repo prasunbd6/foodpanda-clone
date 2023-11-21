e5f863b1b1de66ea17b572f018f2a9af621fd797
prasunbd6 <prasunbd6@gmail.com>
2023-11-21 16:47:13 +0600

    v60
    Process card

--- src/Slices/Allrestaurant.js
+++ src/Slices/Allrestaurant.js
@@ -2,6 +2,7 @@ import { PiStar } from "react-icons/pi";
 import { TbCurrencyTaka } from "react-icons/tb";
 import { GoClock } from "react-icons/go";
 import { MdOutlineDeliveryDining } from "react-icons/md";
+import { LuBadgePercent } from "react-icons/lu";
 
 const Allrestaurant = () => {
   const imgAddress = [
@@ -102,27 +103,61 @@ const Allrestaurant = () => {
         {imgAddress.map((data) => {
           return (
             <>
-              <div className="w-[500px] h-auto md:w-64 border rounded-xl overflow-hidden group" key={data.id}>
-                <img src={data.photo} className="bg-cover w-full h-full duration-700  group-hover:scale-110" alt="" />
-                <div className="flex flex-col py-3 group-hover:shadow-md">
+              <div
+                className="w-[500px] h-auto md:w-64 border rounded-xl group relative"
+                key={data.id}
+              >
+                <div className=" overflow-hidden">
+                  <img
+                    src={data.photo}
+                    className="bg-cover w-full h-full duration-700  group-hover:scale-110"
+                    alt=""
+                  />
+                </div>
+
+                <div className="flex flex-col py-3 group-hover:shadow-sm group-hover:shadow-pink-100 duration-200">
                   <div className="flex justify-between">
                     <p className="px-2 text-xl font-bold">Res Name</p>
                     <div className="flex justify-between items-center gap-1">
-                      <i className="text-pink-600"><PiStar size={13}/></i>
+                      <i className="text-pink-600">
+                        <PiStar size={13} />
+                      </i>
                       <p>2.1</p>
                       <p>(2000+)</p>
                     </div>
                   </div>
                   <div className="flex items-center">
-                    <i className="flex items-center"><TbCurrencyTaka size={20}/></i>
+                    <i className="flex items-center">
+                      <TbCurrencyTaka size={20} />
+                    </i>
                     <p>Category</p>
                   </div>
                   <div className="flex items-center gap-2">
-                    <i className="text-slate-400 px-2"><GoClock size={11}/></i>
+                    <i className="text-slate-400 px-2">
+                      <GoClock size={11} />
+                    </i>
                     <p className="text-slate-400">45 min</p>
-                    <i className="text-pink-400"><MdOutlineDeliveryDining size={20}/></i>
+                    <i className="text-pink-400">
+                      <MdOutlineDeliveryDining size={20} />
+                    </i>
                     <p className="text-pink-400">delivery</p>
                   </div>
+                </div>
+
+                <div className="w-56 h-36 absolute top-3 left-3 flex flex-col gap-1 text-white">
+                  <span className="w-32 flex gap-1 p-1 rounded-lg bg-gradient-to-r from-pink-500 to-pink-700">
+                    <i>
+                      <LuBadgePercent size={15} />
+                    </i>
+                    <p>Free Delivery</p>
+                  </span>
+
+                  <span className="w-44 flex gap-1 p-1 rounded-lg bg-gradient-to-r from-pink-500 to-pink-700">
+                    <i>
+                      <LuBadgePercent size={15} />
+                    </i>
+                    <p>Voucher </p>
+                  </span>
                 </div>
               </div>
             </>
