1bb15f088cf201b7abae6a456edfedd494d931e0
prasunbd6 <prasunbd6@gmail.com>
2023-12-11 16:39:02 +0600

    v98
    process Google Map

--- src/Components/PickUp.js
+++ src/Components/PickUp.js
@@ -1,11 +1,12 @@
-import GoogleMap from '../Slices/GoogleMap'
+import GoogleMapContainer from '../Slices/GoogleMapContainer'
+
 
 const PickUp = () => {
   return (
     <>
     {/* Container */}
     <div className='md:mt-[150px] w-11/12 h-screen mx-auto flex justify-center items-center'>
-     <GoogleMap/>
+     <GoogleMapContainer/>
     </div>
     </>
   )
--- src/Slices/GoogleMapContainer.js
+++ src/Slices/GoogleMapContainer.js
@@ -1,22 +1,28 @@
 import {useJsApiLoader, GoogleMap} from "@react-google-maps/api"
 
 
-const GoogleMap=()=>{
+const GoogleMapContainer=()=>{
 
 const {isLoaded}=useJsApiLoader({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+    libraries: ["places"],
 });
 
 if(!isLoaded){
     return <p className="text-md">Loading...</p>
 }
 
+const center={lat:48.8584, lng:2.2945}
+
     return(
         <>
         {/* Google Map */}
 
             <div className="w-full h-full flex justify-center items-center flex-col">
-                <GoogleMap center={}>
+
+
+
+                <GoogleMap center={center} zoom={15} mapContainerStyle={{width:"100%", height:"100%"}}>
                     {/* Displaying Markers or Directions */}
                 </GoogleMap>
             </div>
@@ -24,4 +30,4 @@ if(!isLoaded){
     )
 }
 
-export default GoogleMap;
+export default GoogleMapContainer;
