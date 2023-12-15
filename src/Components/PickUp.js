98595042d2bd65b55d6c22592e65ba57263b84c6
Prasun Barua <prasunbd6@gmail.com>
2023-12-15 23:16:45 +0600

    v103
    process google map

--- src/Components/PickUp.js
+++ src/Components/PickUp.js
@@ -1,25 +1,45 @@
 import GoogleMapContainer from "../Slices/GoogleMapContainer";
-import { IoCloseSharp } from "react-icons/io5";
+
+import Slides from "../Slices/Slidesdb";
+import { useState } from "react";
 
 const PickUp = () => {
+
+  const [selectedMapLink, setSelectedMapLink] = useState("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3690.4002078432613!2d91.83427857405155!3d22.338512641511638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30ad275bd5a1127d%3A0x93d7e8fa447e57dd!2sSri%20Govinda%20Restaurant!5e0!3m2!1sen!2sbd!4v1702575054332!5m2!1sen!2sbd");
+
+  const handleRestaurantClick = (mapLink) => {
+    setSelectedMapLink(mapLink);
+  };
+
+
+
   return (
     <>
 
       {/* Google Map  Search Box*/}
-      <div className="p-1 md:absolute md:top-[100px] md:left-[430px] md:w-[460px] md:h-20 md:flex md:justify-around bg-pink-400 md:z-10 gap-2">
+      <ul className="px-1 md:absolute md:top-[100px] bg-pink-700 z-10">
 
-        <input type="text" className="w-28 h-6" placeholder="Origin"/>
-        <input type="text" className="w-28 h-6" placeholder="Destination"/>
-        <button className="text-xs px-2 h-6">Route Calculate</button>
-        <i ><IoCloseSharp/></i>
 
-      </div>
+        {
+          Slides.map(i => {
+            return (
+              <>
+                <li key={i.id} onClick={()=>handleRestaurantClick(i.map_link)} className=" cursor-pointer">{i.restaurant_name}</li>
+              </>
+            )
+          })
+        }
+
+
+      </ul>
+
+
 
 
 
       {/* Container */}
       <div className="md:mt-[98px] md:w-11/12 md:h-[600px] mx-auto flex justify-center items-center">
-        <GoogleMapContainer />
+        <GoogleMapContainer selectedMapLink={selectedMapLink}/>
       </div>
     </>
   );
--- src/Slices/GoogleMapContainer.js
+++ src/Slices/GoogleMapContainer.js
@@ -1,37 +1,29 @@
-import Slidesdb from "./Slidesdb"
 
-const GoogleMapContainer = () => {
+
+
+const GoogleMapContainer = ({selectedMapLink}) => {
+
+ 
 
 
   return (
     <>
-
-
-
       {/* Google Map */}
-      <div className="w-full h-full flex justify-center items-center flex-col md:relative">
-
-
-        {/* Displaying Markers or Directions */}
+      <div className="w-full h-full flex justify-center items-center flex-col gap-4 md:relative">
 
         {
-          Slidesdb.map((address) => {
-            return (
-              <>
-                <iframe
-                  src={address.map_link}
-                  
-                  loading="lazy"
-                  referrerpolicy="no-referrer-when-downgrade"
-                  className="w-full h-full"
-                ></iframe>
-              </>
-            )
-          })
+          selectedMapLink && (
+            <iframe
+                
+                src={selectedMapLink}
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+                className="w-full h-full"
+              ></iframe>
+          )
         }
-
-
-
+               
+              
       </div>
     </>
   );
