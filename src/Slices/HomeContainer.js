21c28b489092f3a3b82daaece7189a4dd190a088
Prasun Barua <prasunbd6@gmail.com>
2023-12-22 22:47:25 +0600

    v115
    process home

--- src/Slices/HomeContainer.js
+++ src/Slices/HomeContainer.js
@@ -1,20 +1,52 @@
+import heroImg from "../Assets/refresh-hero-home-bd.png"
+
 export const HomeContainer = () => {
     return (
         <>
-            <div className="md:w-full flex justify-center items-center relative">
+            <div className="w-full flex justify-center items-center lg:relative">
                 {/*Text & Inbox */}
-                <div className="md:w-11/12 flex flex-col justify-start items-center">
-                    <p className="text-2xl text-gray-700 px-2">It's the food and groceries you love, delivered</p>
+                <div className="w-11/12 flex flex-col justify-start items-center">
+                    <p className="text-2xl font-bold lg:text-3xl text-gray-700 px-2">It's the food and groceries you love, delivered</p>
                 </div>
                 {/*Image */}
-                <div className="md:w-11/12 flex justify-center items-center">
-                    <img src="https://images.deliveryhero.io/image/foodpanda/homepage/refresh-hero-home-bd.png?width=1264" className="w-full h-full" alt="" />
+                <div className="w-11/12 h-auto flex justify-center items-center">
+                    <img src={heroImg} className="" alt="" />
                 </div>
             </div>
-            <div className="absolute md:-bottom-7 md:left-9 lg:bottom-24 lg:left-14 xl:left-28 md:w-[694px] lg:w-[450px] bg-white p-4 rounded-xl flex md:flex-col lg:flex-row items-center md:gap-2 lg:gap-4">
-                <input type="text" className="md:w-full lg:w-72 md:h-10 rounded-md focus:outline-none border border-gray-300" />
-                <button className="md:w-full lg:w-28 p-2 rounded-md bg-pink-500 text-white">Find Food</button>
+
+            {/*Input Panel */}
+            <div className="
+2xl:left-[80px]
+2xl:top-[450px]
+xl:left-[70px]
+xl:top-[400px]
+
+            lg:absolute 
+            lg:top-[340px]
+            lg:left-[60px]
+            lg:w-[458px]
+            lg:flex-row
+
+            w-full 
+            bg-white 
+            p-4 
+            rounded-xl 
+            flex 
+            flex-col 
+            items-center 
+            gap-2">
+                <input type="text" className="
+                lg:w-[340px]
+                w-full h-10 rounded-md focus:outline-none border border-gray-300" />
+                <button className="
+                lg:w-[90px]
+                lg:h-[40px]
+                lg:text-md
+                w-full p-1 rounded-md bg-pink-500 text-white">Find Food</button>
+
             </div>
+
+
         </>
     )
 }
