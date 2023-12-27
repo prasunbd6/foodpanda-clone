666473f53591eca604fe46e84a9e9ec05c20aa19
Prasun Barua <prasunbd6@gmail.com>
2023-12-27 22:53:50 +0600

    v125
    process home page

--- src/Slices/HomeContainer.js
+++ src/Slices/HomeContainer.js
@@ -3,14 +3,23 @@ import heroImg from "../Assets/refresh-hero-home-bd.png";
 export const HomeContainer = () => {
   return (
     <>
-      <div className="w-full flex justify-center items-center">
-        <div className="w-auto h-60 flex flex-col justify-around items-start ">
-          <p className="text-2xl font-bold lg:text-3xl text-gray-700 px-2">
+      <div className="w-full h-auto flex justify-center items-center">
+
+        <div className="w-6/12 flex">
+          <p className="text-xl font-bold lg:text-3xl text-gray-700 px-2">
             It's the food and groceries you love, delivered
           </p>
+        </div>
 
-          <div
-            className=" w-[400px]  bg-white mx-2 p-2 rounded-xl flex flex-col justify-start items-center gap-2 
+        <div className="w-6/12 ">
+        <img src={heroImg} className="bg-cover" alt="" />
+        </div>
+
+      </div>
+
+      {/*input*/}
+        <div
+            className=" w-full md:w-[300px] md:flex-row bg-white mx-2 p-3 rounded-xl flex flex-col justify-start items-center gap-2 
           in-md-frame"
           >
             <input
@@ -19,22 +28,10 @@ export const HomeContainer = () => {
         in-md-input"
             />
 
-            <button className="w-full md:w-auto px-4 py-2  rounded-md bg-pink-500 text-white in-md-button">
-              Find
+            <button className="w-full md:w-32 px-4 py-2  rounded-md bg-pink-500 text-white in-md-button md:text-[13px]">
+              Find Food
             </button>
           </div>
-        </div>
-
-        <div className="w-11/12 h-auto flex justify-center items-center">
-          <img src={heroImg} className="bg-cover" alt="" />
-        </div>
-
-        {/*
-   <div className="pt-16">
-        
-        </div>
- */}
-      </div>
     </>
   );
 };
