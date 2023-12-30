991ef9b06872a621b525bdda0faea449ff778c59
Prasun Barua <prasunbd6@gmail.com>
2023-12-30 08:35:54 +0600

    v129
    complete home input

--- src/Slices/HomeContainer.js
+++ src/Slices/HomeContainer.js
@@ -1,17 +1,38 @@
 import heroImg from "../Assets/refresh-hero-home-bd.png";
 import HomeMdInput from "./HomeMdInput";
+import { useState } from "react";
+import { RxCrossCircled } from "react-icons/rx";
 
 export const HomeContainer = () => {
+
+  const [inputValue, setInputValue] = useState("");
+
+  // Function to handle input change
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  // Function to handle input change
+  const clearInput = (e) => {
+    setInputValue("");
+  }
+
   return (
     <>
       <div className="w-full h-auto flex justify-center items-center">
 
-        
+
         <div className="w-7/12 md:h-[250px] flex md:justify-between md:flex-col">
           <p className="text-2xl font-bold lg:text-3xl text-gray-700 px-2">
             It's the food and groceries you love, delivered
           </p>
-          <HomeMdInput/>
+          {/* Input In Medium Device */}
+          <HomeMdInput
+            inputValue={inputValue}
+            handleInputChange={handleInputChange}
+            clearInput={clearInput}
+            RxCrossCircled={RxCrossCircled}
+          />
         </div>
 
         <div className="w-5/12 ">
@@ -20,21 +41,61 @@ export const HomeContainer = () => {
 
       </div>
 
-      {/*input*/}
-      <div
+      {/*input in Small Device*/}
+      <label
         className=" w-full md:w-[300px] md:flex-row bg-white mx-2 p-3 rounded-xl flex flex-col justify-start items-center gap-2 
-          in-md-frame"
+          in-md-frame relative"
       >
         <input
           type="text"
-          className="w-full h-10 rounded-md focus:outline-none border border-gray-300 
-        in-md-input"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Street, Postal code"
+          className="
+          w-full 
+          h-10 
+          px-3
+          rounded-md 
+          focus:outline-none 
+          border 
+          border-gray-300
+          border-opacity-50
+          text-gray-400
+          transition duration-200
+          input-text
+        "
         />
+        <span className="
+                absolute left-1 top-5 
+                mx-5 text-md bg-white text-gray-400 text-opacity-80
+                input-text
+            ">
+          Your street and street number
+        </span>
 
-        <button className="w-full md:w-32 px-4 py-2  rounded-md bg-pink-500 text-white in-md-button md:text-[13px]">
+        {
+          inputValue && (
+            <>
+              <span onClick={clearInput} className=" absolute right-5 top-6 text-gray-600">
+                <RxCrossCircled size={16} />
+              </span>
+            </>
+          )
+        }
+
+        <button className="w-full md:w-32 px-4 py-2  
+        rounded-md bg-pink-500 
+        text-white in-md-button 
+        md:text-[13px]
+        transition duration-300
+        hover:outline-none 
+        hover:ring-2
+        hover:ring-inherit
+        hover:ring-pink-500
+        ">
           Find Food
         </button>
-      </div>
+      </label>
     </>
   );
 };
--- src/Slices/HomeMdInput.js
+++ src/Slices/HomeMdInput.js
@@ -1,23 +1,30 @@
 
 
-const HomeMdInput = () => {
+
+const HomeMdInput = ({inputValue,handleInputChange,clearInput,RxCrossCircled}) => {
+
+    
+
+
     return (
         <>
             {/*input*/}
             <label className="hidden hidden-in-sm md:relative">
                 <input
                     type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
                     placeholder="Street, Postal code"
                     className="
-                    w-full 
+                    w-[530px] 
                     h-10 
                     rounded-md 
                     outline-none 
                     border-2
+                    border-gray-300
                     border-opacity-50
                     px-3
                     text-gray-400
-                    border-gray-300
                     transition duration-200
             "/>
                 <span className="
@@ -27,8 +34,27 @@ const HomeMdInput = () => {
             ">
                     Your street and street number
                 </span>
+                {
+                    inputValue && (
+                        <>
+                            <span onClick={clearInput} className=" absolute right-[155px] top-6 text-gray-600">
+                                <RxCrossCircled size={16} />
+                            </span>
+                        </>
+                    )
+                }
 
-                <button className="w-full md:w-32 px-4 py-2  rounded-md bg-pink-500 text-white in-md-button md:text-[13px]">
+
+                <button className="
+                w-full md:w-32 p-2  
+                rounded-md bg-pink-500 
+                text-white in-md-button 
+                md:text-[13px]
+                hover:outline-none 
+                hover:ring-2
+                hover:ring-inherit
+                hover:ring-pink-500
+                ">
                     Find Food
                 </button>
             </label>
